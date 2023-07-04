74b86b2c2e59d08d0e5b7a0c0c1d927390ff4e1d
mariamakiling <60250267+mariamakiling@users.noreply.github.com>
2023-07-04 20:11:55 +0800

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
-import flet as ft
+def main(page: ft.Page):
+    t = ft.Text(value="Hello, world!", color="green")
+    page.controls.append(t)
+    page.update()
 
-def main (page: ft.Page):
-    page.add(ft.Text('Hello World'))
-
-ft.app(target=main, view=ft.AppView.WEB_BROWSER)
+ft.app(target=main)
